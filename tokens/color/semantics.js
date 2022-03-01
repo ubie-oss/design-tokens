62839f5368335a4e8378603ff874dd027651deed
takanorip <takanori0170@gmail.com>
2022-03-01 16:51:49 +0900

    add figma script
    Signed-off-by: takanorip <takanori0170@gmail.com>

---
+++ tokens/color/semantics.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "Color": {
+    "base"     : { "value": "{Ubie.red.value}" },
+    "secondary": { "value": "{Ubie.green.value}" },
+    "tertiary" : { "value": "{Ubie.gray.100.value}" }
+  }
+}
